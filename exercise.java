ac27caad18383fdc0e84806e619b5b05698649b3
Evridiki Lam <evridiki.lam@gmail.com>
2025-10-22 19:18:00 +0300

    exercise.java check if year is leap

---
+++ exercise.java
@@ -0,0 +1,17 @@
+public class exercise {
+    public static void main( String[]args) {
+        int year;
+        year = 1894;
+        if (year % 4 == 0){
+            System.out.println("Leap year");
+        }
+        else
+        {
+            System.out.println("Regular year");
+        }
+
+        for(int i=0; i<10; i++) {
+            System.out.println(i);
+        }
+    }
+}
