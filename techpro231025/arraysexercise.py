3042e1001a1d773b21b7903ed12e873f43c87dc3
Evridiki Lam <evridiki.lam@gmail.com>
2025-10-23 19:23:31 +0300

    Created folder for code written on 23/10

---
+++ techpro231025/arraysexercise.py
@@ -0,0 +1,9 @@
+scores = [10 , 9.5 , 5 , 6.5 , 7 , 9.2, 3 , 8.5]
+
+for i in range(len(scores)):
+    print(i, scores[i])
+
+subjects = ["Math" , "Physics" , "Literature", "Geometry","Computer Science","Gymnastics","English Literature","Chemistry"]
+
+for i in range(len(subjects)):
+    print(i, subjects[i])
