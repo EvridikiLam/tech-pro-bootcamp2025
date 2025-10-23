3cac3a17cdc4b3cf78e460ac44e05bd12bcc4366
Evridiki Lam <evridiki.lam@gmail.com>
2025-10-23 20:05:45 +0300

    arrays

---
+++ techpro231025/exarrays.java
@@ -0,0 +1,10 @@
+public class exarrays {
+    public static void main(String[] args) {
+        int[] list = {1,2,31,14,25,31,52,621,14,13,521,62};
+
+        //
+        for (int i=list.length-1; i>=0; i=i-1){
+            System.out.println(list[i]);
+        }
+    }
+}
