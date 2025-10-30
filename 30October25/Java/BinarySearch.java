be7afb182f61df2115ab4a0100c67c182cb936b2
Evridiki Lam <evridiki.lam@gmail.com>
2025-10-30 19:23:12 +0200

    Finalized Binary Search

--- 30October25/Java/BinarySearch.java
+++ 30October25/Java/BinarySearch.java
@@ -2,18 +2,44 @@ public class BinarySearch {
 
     public static void main (String[] args) {
         System.out.println("Hello");
-        float[] dataset = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 1, 3, 5, 61, 8, 7,8,2,7};
+        float[] dataset = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170};
 
         System.out.println("Size: " + dataset.length);
         int result;
-        result = DoBinarySearch(4,dataset);
+        result = DoBinarySearch(30,dataset);
         System.out.println(result);
     }
 
     public static int DoBinarySearch (float key, float[] array) {
         
-        while()
-        return -1;
+        int i=0;
+        int start = 0;
+        int stop = array.length-1;
+        int current = 0;
+        int previous = current;
+
+        if (array[start] > key){
+            return -1;
+        }
+        if (array[stop] < key) {
+            return -1;
+        }
+
+        while (array[current] !=key) {
+            System.out.println("Comparing");
+            if (array[current] < key) {
+                start = current;
+            }
+            if (array[current]> key) {
+                stop = current;
+            }
+            previous = current;
+            current = (start + stop)/2;
+            if (previous == current) {
+                return -1;
+            }
+        }
+        return current;
 
     }
 
