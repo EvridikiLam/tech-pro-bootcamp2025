328cd59d9dcf72ed08105aae52631295ef4b6138
Evridiki Lam <evridiki.lam@gmail.com>
2025-10-30 18:47:15 +0200

    Changing the DoBinarySearch class to do a binary search for real

--- 30October25/Java/BinarySearch.java
+++ 30October25/Java/BinarySearch.java
@@ -2,8 +2,9 @@ public class BinarySearch {
 
     public static void main (String[] args) {
         System.out.println("Hello");
-        float[] dataset = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 5};
+        float[] dataset = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 1, 3, 5, 61, 8, 7,8,2,7};
 
+        System.out.println("Size: " + dataset.length);
         int result;
         result = DoBinarySearch(4,dataset);
         System.out.println(result);
@@ -11,13 +12,9 @@ public class BinarySearch {
 
     public static int DoBinarySearch (float key, float[] array) {
         
-        for(int i=0;i<array.length;i++) { 
-            if (array[i] == key) {
-                return i;
-            }
-        }
-
+        while()
         return -1;
+
     }
 
 }
